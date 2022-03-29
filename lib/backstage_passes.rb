c76d790e7aef410c1947f034fa8740ce0d67726c
kat <katharinebrooks@hotmail.co.uk>
2022-03-29 17:04:54 +0100

    backstage passes update quality method when concert has passed working

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -1,9 +1,21 @@
 require_relative 'item.rb'
-class BackstagePasses < Item
-  def initialize(name = "Backstage passes", sell_in, quality )
+class BackstagePass < Item
+  def initialize(name = "Backstage pass", sell_in, quality )
     @name = name
     @sell_in = sell_in
     @quality = quality
+  end
+  def update_quality
+    case @sell_in
+    when - Float::INFINITY..0
+      @quality = 0
+    when 0..5
+      @quality += 3
+    when 6..10
+      @quality += 2
+    else
+      @quality += 1
+    end
   end
 end
 
--- spec/backstage_passes_spec.rb
+++ spec/backstage_passes_spec.rb
@@ -1,5 +1,5 @@
 require 'backstage_passes'
-describe BackstagePasses do 
+describe BackstagePass do 
   subject(:backstage) { described_class.new(15, 20) }
   # it 'has default name backstage passes' do 
   #   expect(subject.name).to eq 'Sulfuras'
@@ -8,7 +8,12 @@ describe BackstagePasses do
   #   expect(subject.sell_in).to eq Float::INFINITY
   # end 
   it 'inherits from item' do 
-  
-  expect(backstage.to_s).to eq "Backstage passes, 15, 20"
+    
+    expect(backstage.to_s).to eq "Backstage pass, 15, 20"
+  end
+  it 'quality is always zero after concert has passed i.e. when <=0' do 
+    bp_post_concert = BackstagePass.new(0, 15)
+    bp_post_concert.update_quality
+    expect(bp_post_concert.to_s).to eq "Backstage pass, 0, 0"
   end
 end
